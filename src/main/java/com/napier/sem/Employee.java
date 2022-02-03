26106e8a1d4602b3fbf15ca330d914e29d571f73
“Ali <40522091@live.napier.ac.uk>
2022-02-02 22:23:09 -0500

    Mysql Database

---
+++ src/main/java/com/napier/sem/Employee.java
@@ -0,0 +1,43 @@
+package com.napier.sem;
+
+/** Getting an Employee
+ We are now ready to add a new method to extract the employee information. To make life easier,
+ we will create an Employee class. To do this, perform the following steps:**/
+
+public class Employee {
+    /**
+     * Employee number
+     */
+    public int emp_no;
+
+    /**
+     * Employee's first name
+     */
+    public String first_name;
+
+    /**
+     * Employee's last name
+     */
+    public String last_name;
+
+    /**
+     * Employee's job title
+     */
+    public String title;
+
+    /**
+     * Employee's salary
+     */
+    public int salary;
+
+    /**
+     * Employee's current department
+     */
+    public String dept_name;
+
+    /**
+     * Employee's manager
+     */
+    public String manager;
+
+}
