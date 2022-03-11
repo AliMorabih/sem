7eeb7aa8166e4fd38ea1c6f022d5e89bef30e7c4
“Ali <40522091@live.napier.ac.uk>
2022-03-10 21:25:14 -0500

    IntegrationTest2

---
+++ src/test/java/com/napier/sem/AppIntegrationExercice.java
@@ -0,0 +1,50 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AppIntegrationExercice {
+
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+
+    }
+    @Test
+    void printSalariesTestContainsNull()
+    {
+        ArrayList<Employee> employess = new ArrayList<Employee>();
+        employess.add(null);
+        app.printSalarybydep(employess);
+    }
+    @Test
+    void printSalaries()
+    {
+        ArrayList<Employee> employees = new ArrayList<Employee>();
+        Employee emp = new Employee();
+        emp.emp_no = 1;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        emp.title = "Engineer";
+        emp.salary = 55000;
+        employees.add(emp);
+        app.printSalarybydep(employees);
+    }
+    /*
+     * Exercise Lab06
+     * This is the test for Display Employee
+     * * */
+
+
+
+}
